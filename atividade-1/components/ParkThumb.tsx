92cc59252b6749ea83796b14c52327913560e62f
Lipe Scheidt <luizfscheidt2@gmail.com>
2023-08-21 19:24:42 -0300

    Settings vscode

---
+++ atividade-1/components/ParkThumb.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+import Colors from "../constants/Colors";
+import { ExternalLink } from "./ExternalLink";
+import { MonoText } from "./StyledText";
+import { Text, View } from "./Themed";
+
+export function ParkThumb() {
+  return <View></View>;
+}
