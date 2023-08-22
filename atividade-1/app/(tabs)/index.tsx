3a379a57a8c4596504febe93d3f01ed5ff8697f3
Francisco Camerini <franciscocamerini@onsign.tv>
2023-08-21 21:20:26 -0300

    Read park data from JSON file

--- atividade-1/app/(tabs)/index.tsx
+++ atividade-1/app/(tabs)/index.tsx
@@ -2,43 +2,13 @@ import { StyleSheet } from "react-native";
 
 import { ParkThumb } from "../../components/ParkThumb";
 import { Text, View } from "../../components/Themed";
-
-const PARKS = [
-  {
-    name: "Beto Carrero",
-    address: "Penha, SC",
-    working_hours: "",
-    lat: "",
-    lng: "",
-  },
-  {
-    name: "Disney",
-    address: "Orlando, Florida",
-    working_hours: "",
-    lat: "",
-    lng: "",
-  },
-  {
-    name: "Hopi Hari",
-    address: "Vinhedo, SP",
-    working_hours: "",
-    lat: "",
-    lng: "",
-  },
-  {
-    name: "Agua Show",
-    address: "Florianópolis, SC",
-    working_hours: "",
-    lat: "",
-    lng: "",
-  },
-];
+import { parks } from "../../assets/parks/data.json";
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      {PARKS.map(function (park) {
-        return <ParkThumb park={park}></ParkThumb>;
+      {parks.map(function (park, i) {
+        return <ParkThumb key={i} park={park}></ParkThumb>;
       })}
     </View>
   );
--- atividade-1/components/ParkThumb.tsx
+++ atividade-1/components/ParkThumb.tsx
@@ -7,9 +7,18 @@ type ParkProps = {
   park: {
     name: string;
     address: string;
-    working_hours: string;
+    open: string; // Opening hour
+    close: string; // Closing hour
     lat: string;
     lng: string;
+    thumb: string; // Thumbnail image url
+    imgs: string[]; // Array of image urls for park detail screen
+    video: string; // Promotional video url
+    social: string[]; // Array of website and social media urls
+    price: Number; // Ticket price
+    buy: string; // Url to buy ticket
+    phone: string;
+    email: string;
   };
 };
 
