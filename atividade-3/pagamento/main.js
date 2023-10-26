7b378b217e633b47e4bb5fb4f7c320a8ef5c7e93
Lipe Scheidt <luizfscheidt2@gmail.com>
2023-10-26 09:07:09 -0300

    Fixing pagamento error

---
+++ atividade-3/pagamento/main.js
@@ -0,0 +1,122 @@
+const express = require("express");
+const app = express();
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const sqlite3 = require("sqlite3");
+
+var db = new sqlite3.Database("dados/dados.db", (err) => {
+  if (err) {
+    console.log("ERRO: não foi possível conectar ao SQLite.");
+    throw err;
+  }
+  console.log("Conectado à base de dados!");
+});
+
+db.run(
+  `CREATE TABLE IF NOT EXISTS "cartao"
+          ("num" TEXT PRIMARY KEY NOT NULL UNIQUE, "cpf" INT NOT NULL UNIQUE)`,
+  [],
+  (err) => {
+    if (err) {
+      console.log("ERRO: não foi possível criar tabela.");
+      throw err;
+    }
+  }
+);
+
+app.post("/cartao", (req, res, next) => {
+  db.run(
+    `INSERT INTO "cartao"("num", "cpf")
+         VALUES (?, ?)`,
+    [req.body.num, req.body.cpf],
+    (err) => {
+      if (err) {
+        console.log("Error: " + err);
+        res.status(500).send("Erro ao cadastrar cartão.");
+      } else {
+        console.log("Cartão cadastrado com sucesso!");
+        res.status(200).send("Cartão cadastrado com sucesso!");
+      }
+    }
+  );
+});
+
+app.get("/cartao", (req, res, next) => {
+  db.all(
+    `SELECT *
+         FROM "cartao"`,
+    (err, result) => {
+      if (err) {
+        console.log("Erro: " + err);
+        res.status(500).send("Erro ao obter dados.");
+      } else {
+        res.status(200).json(result);
+      }
+    }
+  );
+});
+
+app.get("/cartao/:num", (req, res, next) => {
+  db.get(
+    `SELECT *
+         FROM "cartao"
+        WHERE "num" = ?`,
+    req.params.cpf,
+    (err, result) => {
+      if (err) {
+        console.log("Erro: " + err);
+        res.status(500).send("Erro ao obter dados.");
+      } else if (result == null) {
+        console.log("Cartao não encontrado.");
+        res.status(404).send("Cartao não encontrado.");
+      } else {
+        res.status(200).json(result);
+      }
+    }
+  );
+});
+
+app.patch("/cartao/:num", (req, res, next) => {
+  db.run(
+    `UPDATE "cartao"
+          SET "cpf" = COALESCE(?, "cpf"),
+        WHERE "num" = ?`,
+    [req.body.cpf, parseInt(req.params.num)],
+    function (err) {
+      if (err) {
+        console.log(err);
+        res.status(500).send("Erro ao alterar dados.");
+      } else if (this.changes == 0) {
+        console.log("Cartao não encontrado.");
+        res.status(404).send("Cartao não encontrado.");
+      } else {
+        res.status(200).send("Cartao alterado com sucesso!");
+      }
+    }
+  );
+});
+
+app.delete("/cartao/:cpf", (req, res, next) => {
+  db.run(
+    `DELETE FROM "cartao" WHERE "num" = ?`,
+    req.params.num,
+    function (err) {
+      if (err) {
+        res.status(500).send("Erro ao remover Cartao.");
+      } else if (this.changes === 0) {
+        console.log("Cartao não encontrado.");
+        res.status(404).send("Cartao não encontrado.");
+      } else {
+        res.status(200).send("Cartao removido com sucesso!");
+      }
+    }
+  );
+});
+
+let porta = 8084;
+app.listen(porta, () => {
+  console.log("Serviço de usuários em execução: http://127.0.0.1:" + porta);
+});
